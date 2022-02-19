2222e39d2ec1ce5afb02682970b2c5ae51517090
ahmedamine12 <76753712+ahmedamine12@users.noreply.github.com>
2022-02-19 10:49:56 +0100

    Update CriterePrix.java

--- Agence_D'allocation/src/Traitement/CriterePrix.java
+++ Agence_D'allocation/src/Traitement/CriterePrix.java
@@ -1,11 +1,7 @@
 package Traitement;
 import java.io.Serializable;
 
-/**
- * @author ERROUISSI
- * @version 1.0
- * @created 25-déc.-2016 22:07:36
- */
+
 public class CriterePrix implements Critere , Serializable {
 	/**
 	 * 
