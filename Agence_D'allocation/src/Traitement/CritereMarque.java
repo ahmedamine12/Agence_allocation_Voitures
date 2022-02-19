6a465ee7fd4693c3f70441be3f86f6b7a848cedf
ahmedamine12 <76753712+ahmedamine12@users.noreply.github.com>
2022-02-19 10:49:30 +0100

    Update CritereMarque.java

--- Agence_D'allocation/src/Traitement/CritereMarque.java
+++ Agence_D'allocation/src/Traitement/CritereMarque.java
@@ -1,11 +1,7 @@
 package Traitement;
 import java.io.Serializable;
 
-/**
- * @author ERROUISSI
- * @version 1.0
- * @created 25-déc.-2016 22:07:35
- */
+
 public class CritereMarque implements Critere , Serializable {
 	/**
 	 * 
